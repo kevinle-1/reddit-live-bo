7aaf88b2a3557f7c74d62ed8e621b9d3edd5579a
Kevin Le <git@kevle.xyz>
2023-10-10 09:23:26 +0800

    fix url appending

--- live.py
+++ live.py
@@ -43,12 +43,15 @@ def update_entries():
         links = re.findall(URL_REGEX, live_entry_md) # Extract all links
         live_entry_md = re.sub(MD_URL_REGEX, '', live_entry_md) # Remove all markdown links
         
-        msg = f"{live_entry_md.strip()} - {', '.join(links)}" # Add links to send
+        text = live_entry_md.strip() 
+        
+        if links:
+            text = text + '\n' + ', '.join(links) # Add links to end of text
 
         for char in REPLACE_CHARS:
-            msg = msg.replace(char, "")
+            text = text.replace(char, "")
 
-        entries[live_entry_guid] = msg
+        entries[live_entry_guid] = text
 
     return entries
 
@@ -66,6 +69,7 @@ def send_to_webhook(entries_new):
 
 if __name__ == "__main__":
     entries_current_state = update_entries() # Don't send existing entries
+    send_to_webhook(entries_current_state)
 
     while True:
         logger.info("Checking for additional posts...")
